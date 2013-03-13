912739932c4447ab94443a32aa98ed3516b23d33
francispereira <francispereira@7terminals.com>
2013-03-13 15:02:15 +0530

    added ~/var/run and ~/var/spool + basic docs

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,42 +1,31 @@
+# Define appstack::setup
+# Sets up a directory structure for hosting isolated applications
+# ~/apps - JDk, Tomcat, Ant etc
+# ~/hangar - source code
+# ~/log - each application created a folder in here and places its logs in it
+# ~/www - webapp directory
+# ~/var - pid files, spool files etc
+# ~/cron - each cron must create a directory here and run from inside it
+# ~/.ssh - .ssh directory for the user
+
 define appstack::setup ($user, $group) {
-  file { "/home/${user}/apps":
+  File {
     ensure => directory,
     owner  => $user,
     group  => $group,
-    mode   => '644',
   }
 
-  file { "/home/${user}/hangar":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '644',
-  }
+  file { "/home/${user}/apps": mode => '644', }
 
-  file { "/home/${user}/log":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '644',
-  }
+  file { "/home/${user}/hangar": mode => '644', }
 
-  file { "/home/${user}/www":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '644',
-  }
+  file { "/home/${user}/log": mode => '644', }
 
-  file { "/home/${user}/cron":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '644',
-  }
-    file { "/home/${user}/.ssh":
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '700',
-  }
+  file { "/home/${user}/www": mode => '644', }
+
+  file { ["/home/${user}/var", "/home/${user}/var/run", "/home/${user}/var/spool"]: mode => '644', }
+
+  file { "/home/${user}/cron": mode => '644', }
+
+  file { "/home/${user}/.ssh": mode => '700', }
 }
