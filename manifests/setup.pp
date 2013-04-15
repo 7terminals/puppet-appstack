c24f569f4f4a96d66f9d51217c899ce82ee6349c
jude <judebpereira@gmail.com>
2013-04-15 18:43:14 +0530

    initial support for ensure present/absent, osfamily, and tests

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -8,24 +8,80 @@
 # ~/cron - each cron must create a directory here and run from inside it
 # ~/.ssh - .ssh directory for the user
 
-define appstack::setup ($user, $group) {
-  File {
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
+define appstack::setup ($user = undef, $group = undef, $ensure = 'present') {
+  # we support only Debian and RedHat
+  case $::osfamily {
+    Debian  : { $supported = true }
+    RedHat  : { $supported = true }
+    default : { fail("The ${module_name} module is not supported on ${::osfamily} based systems") }
   }
 
-  file { "/home/${user}/apps": mode => '644', }
+  # validate parameters
+  if ($user == undef) {
+    fail('user parameter must be set')
+  }
 
-  file { "/home/${user}/hangar": mode => '644', }
+  if ($group == undef) {
+    fail('group parameter must be set')
+  }
 
-  file { "/home/${user}/log": mode => '644', }
+  if !($ensure in ['absent', 'present']) {
+    fail('ensure parameter must be either absent or present')
+  }
 
-  file { "/home/${user}/www": mode => '644', }
+  if ($ensure == 'present') {
+    File {
+      ensure => directory,
+      owner  => $user,
+      group  => $group,
+    }
 
-  file { ["/home/${user}/var", "/home/${user}/var/run", "/home/${user}/var/spool"]: mode => '644', }
+    file { "/home/${user}/apps":
+      mode => '644',
+    }
 
-  file { "/home/${user}/cron": mode => '644', }
+    file { "/home/${user}/hangar":
+      mode => '644',
+    }
 
-  file { "/home/${user}/.ssh": mode => '700', }
+    file { "/home/${user}/log":
+      mode => '644',
+    }
+
+    file { "/home/${user}/www":
+      mode => '644',
+    }
+
+    file { ["/home/${user}/var", "/home/${user}/var/run", "/home/${user}/var/spool"]:
+      mode => '644',
+    }
+
+    file { "/home/${user}/cron":
+      mode => '644',
+    }
+
+    file { "/home/${user}/.ssh":
+      mode => '700',
+    }
+  } else {
+    # delete all modifications done
+    file { ["/home/${user}/apps", "/home/${user}/hangar", "/home/${user}/log", "/home/${user}/www"]:
+      ensure => 'absent',
+      recurse => true,
+      force   => true,
+    }
+
+    file { ["/home/${user}/var", "/home/${user}/var/run", "/home/${user}/var/spool"]:
+      ensure => 'absent',
+      recurse => true,
+      force   => true,
+    }
+
+    file { ["/home/${user}/cron", "/home/${user}/.ssh"]:
+      ensure => 'absent',
+      recurse => true,
+      force   => true,
+    }
+  }
+
 }
--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,5 @@
-include appstack
+appstack::setup { 'setup_home':
+  user   => 'jude',
+  group  => 'jude',
+  ensure => 'present'
+}
